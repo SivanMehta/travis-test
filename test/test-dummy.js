04221e760cf6714b21433e50a5f1e6de0ef3928f
Sivan Mehta <skmehta@andrew.cmu.edu>
2016-05-02 21:34:15 -0400

    added dummy test

---
+++ test/test-dummy.js
@@ -0,0 +1,11 @@
+var Browser = require("zombie");
+var browser = new Browser({ debug: true });
+var assert = require('chai').assert;
+
+describe("should run the test", function()
+{
+    it("runs the test", function(done)
+    {
+        done();
+    })
+})
