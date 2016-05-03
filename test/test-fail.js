cd580be529e4eecbb8951a176c78c07f60f1dcd9
Sivan Mehta <skmehta@andrew.cmu.edu>
2016-05-03 00:58:06 -0400

    added example of a failing test

---
+++ test/test-fail.js
@@ -0,0 +1,19 @@
+var Browser = require("zombie");
+var assert = require('chai').assert;
+
+describe("A failing test example", function()
+{
+    it("should pass this test", function(done)
+    {
+        assert.isFalse(2 + 2 == 5);
+
+        done();
+    });
+
+    it("should not pass this test", function(done)
+    {
+        assert.isTrue("Your mother" == "a nice lady");
+
+        done();
+    });
+});
